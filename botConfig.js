62db0492e9c377d706158f3fa3a257dc16426503
Despacito98 <94779968+Despacito98@users.noreply.github.com>
2021-11-21 14:01:58 +0530

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "OTExODk0NTMxODM2OTAzNDM0.YZoB9g.S8icfk9KcJyU1hSDAB0gT2SIDIY",
+    prefix: "*",
     admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+        "566652202723049513"
 ],
     debug: true,
-    countChannel: "countChannelID"
+    countChannel: "911483624824651796"
 };
